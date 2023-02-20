64972fd968a7ca2e101f3ce54b89f93810c4cd8b
smakimka <61575866+smakimka@users.noreply.github.com>
2023-02-21 02:37:48 +0300

    stats +

--- src/tg.rs
+++ src/tg.rs
@@ -10,16 +10,20 @@ pub(crate) async fn get_user_string(bot: teloxide::Bot, user: User) -> String {
         let chat = chat.unwrap();
         let username = chat.username();
         if username.is_some() {
-            return format!("User {} [login: {}, sem: {}]\n", user.id, username.unwrap(), user.semester);
+            return format!("User {} [login: {}, sem: {}]", user.id, username.unwrap(), user.semester);
         }
 
         let first_name = chat.first_name();
         let last_name = chat.last_name();
 
         if first_name.is_some() && last_name.is_some() {
-            return format!("User {} [login: {} {}, sem: {}]\n", user.id, first_name.unwrap(), last_name.unwrap(), user.semester);
+            return format!("User {} [login: {} {}, sem: {}]", user.id, first_name.unwrap(), last_name.unwrap(), user.semester);
+        }
+
+        if first_name.is_some() && last_name.is_none() {
+            return format!("User {} [login: {}, sem: {}]", user.id, first_name.unwrap(), user.semester);
         }
     }
 
-    format!("User {} [login: {}, sem: {}]\n", user.id, user.username, user.semester)
+    format!("User {} [login: {}, sem: {}]", user.id, user.username, user.semester)
 }
