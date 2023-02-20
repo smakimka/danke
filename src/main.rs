fc83c24553b495f2a16c0502873ae928ee4ea74d
smakimka <61575866+smakimka@users.noreply.github.com>
2023-02-21 01:13:02 +0300

    stats3

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ enum Command {
     SetSemester { semester: i64 },
     #[command(description = "Получить рейтинг по всем предметам")]
     GetRating,
-    #[command(description = "нет описания")]
+    #[command(description = "для одмина")]
     Stats,
 }
 
